196f140b9ef2af0ef7af579f1ba689c9b63bd98f
김민제 <mingmy00@ssts.co.kr>
2026-06-04 12:41:01 +0900

    fix : 환경변수 네임 수정

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,11 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
+  region: process.env.APP_REGION ?? process.env.AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.APP_ACCESS_KEY_ID ?? process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.APP_SECRET_ACCESS_KEY ?? process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
-export const BUCKET = process.env.S3_BUCKET_NAME!;
+export const BUCKET = process.env.APP_BUCKET_NAME ?? process.env.S3_BUCKET_NAME!;
